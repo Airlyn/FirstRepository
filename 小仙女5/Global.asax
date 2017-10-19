5af3d2c71aca455091b34f733117e2f5ae633997
lenovo <lenovo@LT-PC>
2017-10-19 15:00:48 +0800

    添加项目文件。

---
+++ 小仙女5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="小仙女5.MvcApplication" Language="C#" %>
---
+++ 小仙女5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(小仙女5.Startup))]
+namespace 小仙女5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
